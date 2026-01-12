821cef3025e460e7a8b3b6af8c59448b8a4ba415
yashvanthbalaji <333yashvanthbalaji@gmail.com>
2026-01-12 21:07:54 +0530

    feat: add Reverse array using stack

---
+++ StackProblems/ReverseStack.java
@@ -0,0 +1,33 @@
+package StackProblems;
+import java.util.Scanner;
+
+public class ReverseStack
+{
+    public static void main ( String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size : ");
+        int size = scanner.nextInt();
+        BasicStackOperation obj1 = new BasicStackOperation(size);
+        System.out.println("Enter elements");
+        for(int i=0;i<size;i++)
+        {
+            obj1.push(scanner.nextInt());
+        }
+
+        System.out.println("Reversed Stack");
+        while (!obj1.isEmpty()) {
+            System.out.print(obj1.pop() + " ");
+        }
+        System.out.println();
+
+
+
+
+
+
+
+
+    scanner.close();
+    }
+}
